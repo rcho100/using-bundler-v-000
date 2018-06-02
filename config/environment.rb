9e94cf0d6e67483be72a393a933f97b33b3aab4a
Richard Cho <richardcho100@gmail.com>
2018-06-02 13:56:13 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,3 @@
 require "bundler/setup"
-# require "sinatra"
-# require "pry"
-# require "hashie"
-# require "octokit"
-# require "awesome_print"
 
-# bundler.require(:default)
 Bundler.require(:default, :development)
